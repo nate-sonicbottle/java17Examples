070e47d768d7000f9ebb6f2fb82e5ee943ebfd03
Nathan Phillips <Nathan.Phillips@mettle.co.uk>
2022-04-25 17:43:07 +0100

    Added examples for the records methods

--- src/test/java/com/sonicbottle/record/RecordTest.java
+++ src/test/java/com/sonicbottle/record/RecordTest.java
@@ -1,5 +1,6 @@
 package com.sonicbottle.record;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class RecordTest {
@@ -8,7 +9,30 @@ class RecordTest {
     void test() {
         var bob = new ExampleRecord("Bob", "Jones");
 
-        System.out.println("First Name" + bob.firstName());
-        System.out.println("Last Name " + bob.lastName());
+        System.out.println("First Name: " + bob.firstName());
+        System.out.println("Last Name: " + bob.lastName());
+    }
+
+    @Test
+    void testToString() {
+        var bob = new ExampleRecord("Bob", "Jones");
+        System.out.println("toString(): \t" + bob);
+    }
+
+    @Test
+    void testEquals() {
+        var bob1 = new ExampleRecord("Bob", "Jones");
+        var bob2 = new ExampleRecord("Bob", "Jones");
+
+        Assertions.assertEquals(bob1, bob2);
+    }
+
+    @Test
+    void testHashCode() {
+        var bob1 = new ExampleRecord("Bob", "Jones");
+        System.out.println(bob1.hashCode()); //73832226
+
+        var bob2 = new ExampleRecord("Bob", "Jones");
+        System.out.println(bob2.hashCode()); //73832226
     }
 }
